9c0f9f7c9c8697b2e6f915a045138f6885cadbe0
Weslyn Whitehead <docwes1@gmail.com>
2019-10-15 21:48:02 -0400

    fix the state error by turning onSearchSubmit into an arrow function and then refactoring async

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
     images: []
   };
 
-  async onSearchSubmit(term) {
+   onSearchSubmit = async (term) => {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
@@ -17,13 +17,14 @@ class App extends React.Component {
       }
     });
 
-    console.log(response.data.results);
+    this.setState({images: response.data.results});
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }

784d8b63e548a3d72e43a62c68308244ed6f6ca3
Weslyn Whitehead <docwes1@gmail.com>
2019-10-15 21:30:05 -0400

    intergrated Axios to handle network call to unsplash api and wired up the basic request to return a json list of photo objects.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 import { returnStatement } from "@babel/types";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 748626ce3988969f8f2eabefb60cfacc753509780b5752307f74218d2ed6a8bb"
+      }
+    });
   }
 
   render() {
